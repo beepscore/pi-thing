52c359c1348b9c21f7c47917d024d7c161652b43
beepscore <support@beepscore.com>
2016-06-27 21:44:40 -0700

    Add blink LED. TODO: Test on raspberry pi hardware.

--- webapp/thing_test.py
+++ webapp/thing_test.py
@@ -9,3 +9,11 @@ pi_thing = PiThing()
 # Get the current switch state
 switch = pi_thing.read_switch()
 print('Switch: {0}'.format(switch))
+
+# Blink the LED forever.
+print('Blinking LED (Ctrl-C to stop)...')
+while True:
+    pi_thing.set_led(True)
+    time.sleep(0.5)
+    pi_thing.set_led(False)
+    time.sleep(0.5)
