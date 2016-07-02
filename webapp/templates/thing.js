83ad4eee6a1db346b2ead5b391271bef878df966
beepscore <support@beepscore.com>
2016-07-01 20:05:28 -0700

    In index.html add buttons. Add thing.js

---
+++ webapp/templates/thing.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    %('#led_on').click(function() {
+        console.log('LED on!');
+    });
+    %('#led_off').click(function() {
+        console.log('LED off!');
+    });
+});
