b7c8be70065570564903f769b0e24f7b1a2970b2
beepscore <support@beepscore.com>
2016-07-01 21:33:03 -0700

    In thing.js use jquery function to make POST requests

--- webapp/static/js/thing.js
+++ webapp/static/js/thing.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
     $('#led_on').click(function() {
         console.log('LED on!');
+        $.post('/led/1');
     });
     $('#led_off').click(function() {
         console.log('LED off!');
+        $.post('/led/0');
     });
 });
