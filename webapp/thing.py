126f716effb1c4a0a3ed36b04b020de5936c2502
beepscore <support@beepscore.com>
2016-07-05 20:02:57 -0700

    Make thing.py read and write thread safe by using threading.Lock

--- webapp/thing.py
+++ webapp/thing.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import RPi.GPIO as GPIO
+import threading
 
 LED_PIN = 23
 SWITCH_PIN = 24
@@ -21,15 +22,23 @@ class PiThing(object):
         GPIO.setup(LED_PIN, GPIO.OUT)
         # switch as input
         GPIO.setup(SWITCH_PIN, GPIO.IN)
+        # use prefix _ to show intent treat _lock as "private"
+        # Some Python developers follow this naming convention.
+        self._lock = threading.Lock()
 
     def read_switch(self):
-        """returns true if switch is high, false if switch is low
+        """returns true if switch is high, false if switch is low.
+        Uses lock for thread safety.
         """
-        return GPIO.input(SWITCH_PIN)
+        # 'with' is a context, lock makes method thread safe.
+        with self._lock:
+            return GPIO.input(SWITCH_PIN)
 
 
     def set_led(self, value):
         """Set the LED to the passed in value, True for on, False for off.
+        Uses lock for thread safety.
         """
-        GPIO.output(LED_PIN, value)
+        with self._lock:
+            GPIO.output(LED_PIN, value)
 
