a83eaacb39678567dfa348f9cfb389be89f1dfd3
beepscore <support@beepscore.com>
2016-07-20 21:58:30 -0700

    In index.html add div with canvas id dht_chart. In thing.js add dhtChart with sample data.

--- webapp/static/js/thing.js
+++ webapp/static/js/thing.js
@@ -12,6 +12,51 @@ $(document).ready(function() {
     });
 });
 
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40],
+            spanGaps: false,
+        }
+    ]
+};
+
+var options = {
+    scales: {
+        xAxes: [{
+            type: 'linear',
+            position: 'bottom'
+        }]
+    }
+};
+
+// use jquery get()
+var ctx = $("#dht_chart").get(0).getContext("2d");
+// get first node in the jQuey collection
+var dhtChart = Chart.Line(ctx, {
+    data: data,
+    options: options
+});
+
 // Setup server sent event endpoint for /switch
 // https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events
 
